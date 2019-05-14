7c293c078557366a98b14f88ff310c23c2f15d83
Bluewind12 <zero18030@gmail.com>
2019-05-14 18:14:14 +0900

    オプションメニューの実装

--- app/src/main/java/momonyan/mahjongrecorder/MainActivity.kt
+++ app/src/main/java/momonyan/mahjongrecorder/MainActivity.kt
@@ -2,11 +2,45 @@ package momonyan.mahjongrecorder
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_option_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menuPlayerData -> {
+                //TODO プレイヤー個人の成績を出力するページへ（グラフとかあるといいかもしれぬ）
+                Log.d("Menu",item.itemId.toString())
+                return true
+            }
+            R.id.menuResult -> {
+                //TODO 存在するプレイヤーの成績を簡易出力
+                Log.d("Menu",item.itemId.toString())
+                return true
+            }
+            R.id.menuPrivacy -> {
+                //TODO プライバシーポリシーに飛ばす
+                Log.d("Menu",item.itemId.toString())
+                return true
+            }
+            R.id.menuReview -> {
+                //TODO レビューへ飛ばす
+                Log.d("Menu",item.itemId.toString())
+                return true
+            }
+        }
+        return true
     }
 }
