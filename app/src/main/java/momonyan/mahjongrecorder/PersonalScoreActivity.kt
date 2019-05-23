1b5f69aa891a23696f0191a2107a77acd11ae298
Bluewind12 <zero18030@gmail.com>
2019-05-24 02:21:44 +0900

    グラフの修正 サイズ、表示

--- app/src/main/java/momonyan/mahjongrecorder/PersonalScoreActivity.kt
+++ app/src/main/java/momonyan/mahjongrecorder/PersonalScoreActivity.kt
@@ -3,11 +3,13 @@ package momonyan.mahjongrecorder
 import android.arch.persistence.room.Room
 import android.graphics.Color
 import android.graphics.DashPathEffect
+import android.graphics.Point
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.View
+import android.view.WindowManager
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import com.github.mikephil.charting.components.XAxis
@@ -59,12 +61,23 @@ class PersonalScoreActivity : AppCompatActivity() {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 dataSet(personalSpinner.selectedItem.toString())
             }
-
         }
+
+
     }
 
     fun dataSet(name: String) {
         if (name != "" && name != "プレイヤーを選択してください") {
+            //グラフのサイズの動的変更
+            val wm = getSystemService(WINDOW_SERVICE)
+            if (wm is WindowManager) {
+                val disp = wm.defaultDisplay
+                val size = Point()
+                disp.getSize(size)
+                lineChart.layoutParams.height = size.x * 2 / 3
+                lineChart2.layoutParams.height = size.x * 2 / 3
+            }
+
             playerAppDataBase.playerDataBaseDao().getPoint(name).observe(this, android.arch.lifecycle.Observer { data ->
                 val maxPoint = data!!.max()!! * 100
                 val minPoint = data.min()!! * 100
@@ -146,7 +159,7 @@ class PersonalScoreActivity : AppCompatActivity() {
         val data = mutableListOf<Float>()
         var sumData = 0.0f
         for (i in 0 until basicData.size) {
-            sumData += basicData[i] * 100
+            sumData += basicData[i]
             data.add(sumData / (i + 1))
             Log.d("Data", "$i - $sumData")
         }
@@ -178,39 +191,57 @@ class PersonalScoreActivity : AppCompatActivity() {
 
         // add data
         val values = ArrayList<Entry>()
+        val values2 = ArrayList<Entry>()
 
         for (i in data.indices) {
-            values.add(Entry(i.toFloat(), data[i], null, null))
+            values.add(Entry(i.toFloat(), data[i] * 100, null, null))
+            values2.add(Entry(i.toFloat(), basicData[i].toFloat() * 100, null, null))
         }
 
         val set1: LineDataSet
+        val set2: LineDataSet
 
         if (mChart.data != null && mChart.data.dataSetCount > 0) {
-
             set1 = mChart.data.getDataSetByIndex(0) as LineDataSet
             set1.values = values
+            set2 = mChart.data.getDataSetByIndex(0) as LineDataSet
+            set2.values = values
             mChart.data.notifyDataChanged()
             mChart.notifyDataSetChanged()
         } else {
             // create a dataset and give it a type
             set1 = LineDataSet(values, "点数平均")
-
             set1.setDrawIcons(false)
             set1.color = Color.BLUE
-            set1.setCircleColor(Color.BLACK)
-            set1.lineWidth = 1f
-            set1.circleRadius = 3f
+            set1.setCircleColor(Color.BLUE)
+            set1.lineWidth = 3f
+            set1.circleRadius = 5f
             set1.setDrawCircleHole(false)
             set1.valueTextSize = 0f
             set1.setDrawFilled(true)
             set1.formLineWidth = 1f
             set1.formLineDashEffect = DashPathEffect(floatArrayOf(10f, 5f), 0f)
             set1.formSize = 15f
+            set1.fillColor = Color.argb(0, 255, 255, 255)
 
-            set1.fillColor = Color.CYAN
+            set2 = LineDataSet(values2, "取得点数")
+            set2.setDrawIcons(false)
+            set2.color = Color.RED
+            set2.setCircleColor(Color.RED)
+            set2.lineWidth = 3f
+            set2.circleRadius = 5f
+            set2.setDrawCircleHole(false)
+            set2.valueTextSize = 0f
+            set2.setDrawFilled(true)
+            set2.formLineWidth = 1f
+            set2.formLineDashEffect = DashPathEffect(floatArrayOf(10f, 5f), 0f)
+            set2.formSize = 15f
+            set2.fillColor = Color.argb(0, 255,255,255)
+
 
             val dataSets = ArrayList<ILineDataSet>()
             dataSets.add(set1) // add the datasets
+            dataSets.add(set2) // add the datasets
 
             // create a data object with the datasets
             val lineData = LineData(dataSets)
@@ -220,6 +251,5 @@ class PersonalScoreActivity : AppCompatActivity() {
 
             mChart.animateX(2500)
         }
-
     }
 }
