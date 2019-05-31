3ddef5c56f3b80892de6c53023d8511bca581abb
bluewind_mac <zero18030@gmail.com>
2019-05-31 12:20:17 +0900

    ADのデータの変更。 テストデータからの変更

--- app/src/main/java/momonyan/mahjongrecorder/datalist/ItemDataClass.kt
+++ app/src/main/java/momonyan/mahjongrecorder/datalist/ItemDataClass.kt
@@ -6,33 +6,4 @@ class ItemDataClass(
     val dPoint: MutableList<Int>,
     val dResult: MutableList<Double>,
     val dDate: String
-) {
-    //Getter
-    //Player
-    fun getPlayerName(): MutableList<String> {
-        return dName
-    }
-
-    fun getPlayerName(num :Int):String{
-        return dName[num]
-    }
-
-    //Point
-    fun getpoint(): MutableList<Int> {
-        return dPoint
-    }
-
-    fun getpoint(num :Int):Int{
-        return dPoint[num]
-    }
-
-    //Result
-    fun getresultPoint(): MutableList<Double> {
-        return dResult
-    }
-
-    fun getresultPoint(num: Int): Double {
-        return dResult[num]
-    }
-
-}
+)
